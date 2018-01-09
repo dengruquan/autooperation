25c5c4c8d4108e43a3f6936557207c0cf3fd500c
centos_game <kg@centos_game.(none)>
2018-01-09 14:51:19 +0800

    提交ansible2.4 inventory脚本

--- jobs/ansible_runner/inventorys.py
+++ jobs/ansible_runner/inventorys.py
@@ -11,25 +11,25 @@ class JMSInventoryManager(InventoryManager):
         单机：[{ip:, port:, vars: {}}, {}]or 
         组：{group1:[{ip:, port:, vars: {}}, {}], }
         '''
-            loader = DataLoader()
-            super(JMSInventoryManager, self).__init__(loader = loader)
-            self.parse_inventory(hosts)
+        loader = DataLoader()
+        super(JMSInventoryManager, self).__init__(loader = loader)
+        self.parse_inventory(hosts)
             
             
-    def parse_inventory(hosts):
+    def parse_inventory(self, hosts):
         
         try:
-            if instance(hosts, list):
-                myaddhost(hosts)
-            elif instance(hosts, dict):
-                for groupname, host_list in hosts.iteritems():
-                    myaddhost(hosts, groupname)
+            if isinstance(hosts, list):
+                self.myaddhost(hosts)
+            elif isinstance(hosts, dict):
+                for groupname, host_list in hosts.items():
+                    self.myaddhost(host_list, groupname)
         except:
             print("JMSInventoryManager error")
             pass
             
             
-    def myaddhost(self, host_list, group = None):
+    def myaddhost(self, host_list, groupname = None):
         '''
         按照自己的规则组织inventory
         @parameter host_list: 主机列表字典
@@ -43,18 +43,27 @@ class JMSInventoryManager(InventoryManager):
         for hostinfo in host_list:
             ip = hostinfo["ip"]
             port = hostinfo["port"]
-            inventory.add_host(ip, group, port)
+            inventory.add_host(ip, groupname, port)
 
 
 if __name__ == "__main__":
+    ip = "192.168.15.20"
+    port = 22
+    username = ""
+    password = ""
     hosts = [
         {
             "hostname": 'host',
-            "ip": ip,
+            "ip": ip ,
             "port": port,
             "username": username,
             "password": password,
         },
     ]
-    obj = JMSInventoryManager(hosts)
-    print(obj.hosts)
+
+    hosts_dict = {
+	"kg" : hosts
+	}
+
+    obj = JMSInventoryManager(hosts_dict)
+    print(obj.hosts, obj.groups)
